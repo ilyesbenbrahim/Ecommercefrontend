7ec664b92cc106ecf694c0e3e0386ce10c56f8e9
ilyesbenbrahim <benbrahim1.ilyes@gmail.com>
2024-05-16 17:55:32 -0400

    remove product

--- src/Pages/product/Product.jsx
+++ src/Pages/product/Product.jsx
@@ -2,60 +2,71 @@ import NavBar from "../../components/NavBar";
 import Topbar from "../../components/topbare/Topbar";
 import "./Product.css";
 import imageproduct from "../../assets/imgnike.jpg";
-// import tttt from "../../assets/imgproduct.jpg";
-// import imageun from "../../assets/imageun.jpg";
-// import imagedeux from "../../assets/imdedeux.jpg";
-// import imagetatrios from "../../assets/imget.jpg";
-// import imagequatre from "../../assets/imgequatre.jpg";
-// import imagecinq from "../../assets/imagecinq.jpg";
-// import imagesix from "../../assets/imagesix.jpg";
-// import imagesept from "../../assets/imgesept.jpg";
-// import imagehuit from "../../assets/imgehuit.jpg";
-// import axios from 'axios'
-
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+
 const Product = () => {
   const [products, setProducts] = useState([]);
+
   useEffect(() => {
-    axios.get("http://localhost:5000/api/products").then((res) => {
-      setProducts(res.data);
-    }).catch((err)=>console.log(err))
+    fetchProducts();
+    console.log(products)
   }, []);
-  console.log(products)
+
+  const fetchProducts = () => {
+    axios.get("http://localhost:5000/api/products")
+      .then(res => {
+        setProducts(res.data);
+      })
+      .catch(err => console.error('Error fetching products:', err));
+  };
+
+  const deleteProduct = (id) => {
+    axios.delete(`http://localhost:5000/api/products/${id}`)
+      .then(() => {
+        alert('Product deleted successfully');
+        // Refresh the product list by filtering out the deleted product
+        setProducts(products.filter(product => product._id !== id));
+      })
+      .catch(err => {
+        console.error('Error deleting product:', err);
+        alert('Failed to delete product');
+      });
+  };
+
   return (
     <>
-      <div>
-        <Topbar></Topbar>
-        <NavBar></NavBar>
+    <div>
+      <Topbar></Topbar>
+      <NavBar></NavBar>
+      <img
+        className="image-product"
+        src={imageproduct}
+        alt="Description of the logo"
+      />
+      <h1>display: grid</h1>
+      <p>Use display: grid; to make a block-level grid container:</p>
+
+      <div className="grid-container">
+        {products.map((product)=>(
+        <>
+        <p>{product.title}</p>
+        <p>{product.prixduproduit}</p>
+        <p>{product.quantitedisponible}</p>
         <img
-          className="image-product"
-          src={imageproduct}
-          alt="Description of the logo"
-        />
-        <h1>display: grid</h1>
-        <p>Use display: grid; to make a block-level grid container:</p>
+              src={product.image}
+              height={200}
+              width={200}
+            />
+        <button onClick={()=>deleteProduct(product._id)}>delete</button>
 
-        <div className="grid-container">
-          {products.map((product)=>(
-          <>
-          <p>{product.title}</p>
-          <p>{product.prixduproduit}</p>
-          <p>{product.quantitedisponible}</p>
-          <img
-                src={product.image}
-                height={200}
-                width={200}
-              />
-
-          
-          </>
-          ))}
-          
-        </div>
+        
+        </>
+        ))}
+        
       </div>
-    </>
+    </div>
+  </>
   );
 };
 
